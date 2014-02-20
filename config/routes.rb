1dbaba8ff29cd4439422808e9d9809e3056f040e
Nathan Cooper <npcooper@gmail.;com>
2014-02-19 18:09:33 -0700

    Added welcome screen, and configured it to to accept the default index.htm* calls.

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,8 @@ FinancialCalculator::Application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  root 'welcome#index'
+  get '/index', to: 'welcome#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
