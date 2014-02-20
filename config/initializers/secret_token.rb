dda0e8650f6fa915ed9b6322cb10f99e36937e7d
Nathan Cooper <npcooper@gmail.;com>
2014-02-19 17:17:42 -0700

    Initial check-in

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+FinancialCalculator::Application.config.secret_key_base = '31094448216c0bf97bdc6d468b0c5b93d790bd0f65c39cbe5194362fcdab764566d2717e332c2b19238a5ff3d1aa4a413ad5c3f46c8d6b0db8cd8edafb102801'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+FinancialCalculator::Application.config.session_store :cookie_store, key: '_FinancialCalculator_session'
