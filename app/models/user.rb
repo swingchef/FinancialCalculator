0924c369e658379cdbe7977f7ba231e05deeafda
Nathan Cooper <npcooper@gmail.;com>
2014-03-13 12:10:23 -0600

    TODO Item I can't do right now.

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,5 @@ class User < ActiveRecord::Base
   validates :email, presence: true, format: { with: VALID_EMAIL_REGEX }, uniqueness: {case_sensitive: false}
 
   before_save { self.email = email.downcase}
+  # TODO Make the database force unique emails as well.  This can be done by adding an index to the "email" column, and then force unique
 end
