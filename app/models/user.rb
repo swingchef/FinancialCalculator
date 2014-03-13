f10afc85e7e9dffd51a27f9142bbbc6e24edbc85
Nathan Cooper <npcooper@gmail.;com>
2014-03-12 22:56:22 -0600

    Added validation code to make sure the name is at least 5 characters long.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,4 @@
 class User < ActiveRecord::Base
-  validates :name, presence: true
+  validates :name, presence: true, length: {minimum: 5}
   validates :email, presence: true
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -8,6 +8,13 @@ class UserTest < ActiveSupport::TestCase
     assert(user.save == false, "User should not have been saved.")
     user.email = "sam@mcgee.com"
     assert(user.save, "User should have been saved, and wasn't")
+
+    # Test name length validation
+    user = User.new(name: "abc", email: "user@example.com")
+    assert(user.save == false, "User should not have been saved with a 3-letter name.")
+    user.name = "abcde"
+    assert(user.save, "User should have been saved, and wasn't")
+
   end
   # test "the truth" do
   #   assert true
