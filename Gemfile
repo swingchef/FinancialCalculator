a6b49b81543d596d3507dfc711ea5df4f062fcab
Nathan Cooper <npcooper@gmail.;com>
2014-03-14 09:11:21 -0600

    Added bcrypt-hashed passwords to User

--- Gemfile
+++ Gemfile
@@ -3,6 +3,9 @@ source 'https://rubygems.org'
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '4.0.3'
 
+# Bcrypt as the password-hasher
+gem 'bcrypt-ruby', '3.1.5'
+
 # Use sqlite3 as the database for Active Record
 gem 'sqlite3'
 
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,43 +1,57 @@
 require 'test_helper'
-require 'pp'
 
 class UserTest < ActiveSupport::TestCase
   test "user validation" do
-    user = User.new
-    assert(user.save == false, "User should not have been saved.")
+    user = createValidUser()
+    user.name = ""
+    assert(user.save == false, "User should require a non-empty name before saving.")
     user.name = "Sam McGee"
-    assert(user.save == false, "User should not have been saved.")
+    user.email = ""
+    assert(user.save == false, "User should require a non-empty email before saving.")
     user.email = "sam@mcgee.com"
-    assert(user.save, "User should have been saved, and wasn't")
+    user.password = nil
+    user.password_confirmation = nil
+    user.password_digest = nil
+    assert(user.save == false, "User should require a non-empty password before saving.")
+    user.password = "mypassword"
+    user.password_confirmation = ""
+    assert(user.save == false, "User should not be saved without a matching password_confirmation.")
+    user.password = "mypass"
+    user.password_confirmation = "mypass"
+    assert(user.save == false, "User should not be saved when the password is less than eight characters.")
+    user.password = "mypassword"
+    user.password_confirmation = "mypassword"
 
-    pp ('Check 1')
     # Test name length validation
-    user = User.new(name: "abc", email: "user@example.com")
+    user.name = "abc"
     assert(user.save == false, "User should not have been saved with a 3-letter name.")
     user.name = "abcde"
+
+    # Finally, ensure a correct save works
+    user.email = "yabba@DABBA.com"
     assert(user.save, "User should have been saved, and wasn't")
-
-    # Test for bad email
-    user = User.new(name: "Mark McGee", email: "usermchgee")
-
-    assert(user.save == false, "User should not have been saved with a bad email.")
-
-    user.email = "sam@mgee2.com"
-    assert(user.save, "User should have been saved, and wasn't")
-
-    # Test for unique emails
-    user = User.new(name: "Fred Wilber", email: "fred@foo.com")
-    assert(user.save, "User was not saved when he should have")
-    user = User.new(name: "Fred Wilber2", email: "fred@fOo.com")
-    assert(user.save == false, "A duplicate user was saved, when he shouldn't have been.")
 
     # Test ensure emails are properly lower-cased
-    user = User.new(name: "Fred Wilber", email: "fred@BAR.com")
+    assert(user.email == "yabba@dabba.com", "Email was not correctly lower-cased on save")
+
+    # Test for bad email
+    user.email = "usermchgee"
+    assert(user.save == false, "User should not have been saved with a bad email.")
+    user.email = "sam@mgee2.com"
+
+
+    # Test for unique emails
+    user = createValidUser()
+    user.email = "not@unique.com"
     assert(user.save, "User was not saved when he should have")
-    puts user.email
-    assert(user.email == "fred@bar.com", "Email was not lower-cased on save")
+    user = createValidUser()
+    user.email = "not@unique.com"
+    assert(user.save == false, "A duplicate user was saved, when he shouldn't have been.")
+
   end
-  # test "the truth" do
-  #   assert true
-  # end
+
+  def createValidUser()
+    user = User.new(name: "Fred Wilber", email: "fred@bar.com", password: "mypassword", password_confirmation: "mypassword")
+    return user
+  end
 end
