72165813d269ec8c50dd59ce9a1d55a6bfcc9990
Nathan Cooper <npcooper@gmail.;com>
2014-03-14 09:30:07 -0600

    Slight re-formatting of the User model code

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,12 +1,17 @@
 class User < ActiveRecord::Base
-  validates :name, presence: true, length: {minimum: 5}
+  validates :name, presence: true,
+                   length: {minimum: 5}
+
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-  validates :email, presence: true, format: { with: VALID_EMAIL_REGEX }, uniqueness: {case_sensitive: false}
+  validates :email, presence: true,
+                    format: { with: VALID_EMAIL_REGEX },
+                    uniqueness: {case_sensitive: false}
 
   # Password-related fields
   has_secure_password
   # validates :password, presence: { on: create }, length: {minimum: 8}
-  validates :password, length: {minimum: 8}, allow_nil: true
+  validates :password, length: {minimum: 8},
+                       allow_nil: true
 
   before_save { self.email = email.downcase}
   # TODO Make the database force unique emails as well.  This can be done by adding an index to the "email" column, and then force unique
