87711395907b6da9a515ec0975e3a5d03f198557
Nathan Cooper <npcooper@gmail.;com>
2014-03-31 09:17:00 -0600

    Added validation and tests for the validation fields

--- app/models/debt.rb
+++ app/models/debt.rb
@@ -1,2 +1,17 @@
 class Debt < ActiveRecord::Base
+	# Validation for the name field
+	validates :name, presence: true,
+									 length: {minimum: 1}
+
+	# Validation for the amount field
+	validates :amount, presence: true,
+										 :numericality => { :greater_than_or_equal_to => 0 }
+
+	# Validation for the interest field
+	validates :interest_rate, presence: true,
+										 :numericality => { :greater_than_or_equal_to => 0 }
+
+	# Validation for the minimum monthly payment field
+	validates :min_monthly_payment, presence: true,
+										 :numericality => { :greater_than_or_equal_to => 0 }
 end
--- test/models/debt_test.rb
+++ test/models/debt_test.rb
@@ -1,7 +1,37 @@
 require 'test_helper'
 
 class DebtTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+	test "Initialization rules" do
+		d = makeDefaultDebt()
+		d.name = ""
+		assert(d.save == false, "Should not have saved debt with empty name")
+		d.name = "Good name"
+
+		d.amount = -100
+		assert(d.save == false, "Should not have saved debt with a negative amount")
+		d.amount = 1000
+
+		d.interest_rate = -0.10
+		assert(d.save == false, "Should not have saved debt with a negative interest rate")
+		d.interest_rate = 0.12
+
+		d.min_monthly_payment = -10
+		assert(d.save == false, "Should not have saved debt with negative monthly payment")
+		d.min_monthly_payment = 10
+		assert(d.save == true, "Should have saved correctly, but didn't")
+
+	end
+
+	# Creates a default debt, for the sake of convenience
+	def makeDefaultDebt
+		d = Debt.new
+		d.name = "New debt name"
+		d.amount = 1000
+		d.interest_rate = 0.12
+		d.min_monthly_payment = 25
+		return d
+	end
+	# test "the truth" do
+	#   assert true
+	# end
 end
