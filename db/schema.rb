f3388e39d67286d9c778b5c0f9009ca4825d9e6a
Nathan Cooper <npcooper@gmail.;com>
2014-03-31 08:57:59 -0600

    Schema upload.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140322212738) do
+ActiveRecord::Schema.define(version: 20140331143416) do
+
+  create_table "debts", force: true do |t|
+    t.string   "name"
+    t.integer  "amount"
+    t.float    "interest_rate"
+    t.integer  "min_monthly_payment"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "users", force: true do |t|
     t.string   "name"
