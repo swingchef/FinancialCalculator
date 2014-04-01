8e215fcf0917fffdc35fa4195731e307dde890a0
Nathan Cooper <npcooper@gmail.;com>
2014-03-31 21:47:21 -0600

    Test for user_id Added a test to ensure that the Schedule enforces having something in user_id before saving

--- app/models/schedule.rb
+++ app/models/schedule.rb
@@ -1,3 +1,5 @@
 class Schedule < ActiveRecord::Base
   belongs_to :user
+
+	validates :user_id, presence: true
 end
--- test/models/schedule_test.rb
+++ test/models/schedule_test.rb
@@ -1,6 +1,13 @@
 require 'test_helper'
 
 class ScheduleTest < ActiveSupport::TestCase
+	test "Schedule requires User" do
+		s = Schedule.new
+		assert(s.save == false, "Should not have saved schedule without a valid user_id")
+		one = User.find_by(name: "MyString")
+		s.user_id = one.id
+		assert(s.save == true, "Should have saved correctly")
+	end
   # test "the truth" do
   #   assert true
   # end
