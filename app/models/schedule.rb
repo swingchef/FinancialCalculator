74d7be95a4498a7d72b01b6d37f4644aab31574d
Nathan Cooper <npcooper@gmail.;com>
2014-03-31 22:33:32 -0600

    Validates Schedule has valid User Added test to validate that the Schedule has a proper User assigned.

--- app/models/schedule.rb
+++ app/models/schedule.rb
@@ -2,4 +2,5 @@ class Schedule < ActiveRecord::Base
   belongs_to :user
 
 	validates :user_id, presence: true
+	validates_presence_of :user
 end
--- test/models/schedule_test.rb
+++ test/models/schedule_test.rb
@@ -4,11 +4,13 @@ class ScheduleTest < ActiveSupport::TestCase
 	test "Schedule requires User" do
 		s = Schedule.new
 		assert(s.save == false, "Should not have saved schedule without a valid user_id")
+
+		# use a bad user_id
+		s.user_id = "5555555"
+		assert(s.save == false, "Invalid user_id should have failed the save")
+
 		one = User.find_by(name: "MyString")
 		s.user_id = one.id
 		assert(s.save == true, "Should have saved correctly")
 	end
-  # test "the truth" do
-  #   assert true
-  # end
 end
