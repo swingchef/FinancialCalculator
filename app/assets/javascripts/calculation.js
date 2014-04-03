93be5297ce6c6407e7c664c87046792b312aece8
Nathan Cooper <npcooper@gmail.;com>
2014-04-02 21:55:03 -0600

    Fix the calculate methods (because I broke them) Moved the financial functions in the FC.calculate object Added some comments to clarify variables (or ask for clarification) Temporarily removed verification (strictly speaking, the server is already doing this, and we probably want the end-all calculate function to get these varaibles from the server, so we can calculate stored values directly). I'm sorry if I broke anything; please let me know.

--- app/assets/javascripts/calculation.js
+++ app/assets/javascripts/calculation.js
@@ -10,6 +10,10 @@ var FC = FC === undefined ? {} : FC;
 
 FC.calculate = {
 
+	// This is the amount that we want to initially use
+	// for monthly payments based on the user's yearly salary
+	PAYMENT_PERCENT_OF_YEARLY_SALARY: .01,
+
 	// Creates a <p><div><row of debt inputs></div></p> that is self-contained
 	// Returns: jQuery <p> element 
 	createDebtRow: function() {
@@ -54,8 +58,69 @@ FC.calculate = {
 			.append(removeButton);
 
 		return p;
+	},
+
+	// nper function
+	// TODO What does it do?
+	nper:			function(rate, per, pmt, pv, fv) {
+		fv = parseFloat(fv);
+		pmt = parseFloat(pmt);
+		pv = parseFloat(pv);
+		per = parseFloat(per);
+		if ((per == 0 ) || (pmt == 0 )) {
+			alert("Why do you want to test me with zeros?");
+			return (0);
+		}
+		rate = eval((rate) / (per * 100));
+		if (rate == 0)// Interest rate is 0
+		{
+			nper_value = -(fv + pv) / pmt;
+		} else {
+			nper_value = Math.log((-fv * rate + pmt) / (pmt + rate * pv)) / Math.log(1 + rate);
+		}
+		nper_value = FC.calculate.conv_number(nper_value, 2);
+		return (nper_value);
+	},
+
+	// FutureValue Function
+	// TODO What does it do?
+	fv:				function(rate, per, nper, pmt, pv) {
+		nper = parseFloat(nper);
+		pmt = parseFloat(pmt);
+		pv = parseFloat(pv);
+		rate = eval((rate) / (per * 100));
+		if ((pmt == 0 ) || (nper == 0 )) {
+			alert("Why do you want to test me with zeros?");
+			return (0);
+		}
+		if (rate == 0)// Interest rate is 0
+		{
+			fv_value = -(pv + (pmt * nper));
+		} else {
+			x = Math.pow(1 + rate, nper);
+			// y = Math.pow(1 + rate, nper);
+			fv_value = -(-pmt + x * pmt + rate * x * pv ) / rate;
+		}
+		fv_value = FC.calculate.conv_number(fv_value, 2);
+		return (fv_value);
+	},
+
+	// This function is from David Goodman's Javascript Bible.
+	// TODO What does it do?
+	conv_number:	function(expr, decplaces) {
+		var str = "" + Math.round(eval(expr) * Math.pow(10, decplaces));
+		while (str.length <= decplaces) {
+			str = "0" + str;
+		}
+		var decpoint = str.length - decplaces;
+		return (str.substring(0, decpoint) + "." + str.substring(decpoint, str.length));
+
 	}
 }
+
+/////////////////////////////////
+//	Document.Ready
+/////////////////////////////////
 $(document).ready(function() {
 
 	// NEED TO STILL CREATE A FOR LOOP WHICH STARTS AROUND 20 -> 1, CHECKS FOR dname_.value, when found, sets i = .value and breaks
@@ -80,106 +145,52 @@ $(document).ready(function() {
 		// Resets all of the input fields to a non-error state
 		$('input').removeClass('error');
 
-		var payment = parseFloat($("input[name=\"income_input\"]").val())/12 * .01 ;
-		alert("This is your income-based payment: "+ payment);
-		//TODO add in validation to see if income has value
-		for ( i = 100; i > 1; i--) {// this for loop assumes that the user will have no more than 100 inputs, increase var i to desired amount
-			// for loop is finding
-			// TODO Add a class to designate inputs, so we don't need to use the magic number, 100
-			var testerInput = document.getElementsByName("dname_" + i);
-			if (testerInput.length > 0) {
-				break;
-			}// end if
-		}// end for
+		// Get the yearly salary and derive the the monthly payment
+		// TODO add in validation to see if income has value
+		var yearlySalary = $('#yearlySalary').val();
+		var payment = parseFloat(yearlySalary)/12 * FC.calculate.PAYMENT_PERCENT_OF_YEARLY_SALARY;
+		alert("This is your initial income-based payment: "+ payment);
 
-		//alert("number of input rows is: " + i);
-		// TODO take this out when debug.complete
-		var arrayRowCounter = 0;
-		for (var z = 1; z < i + 1; z++) {// this for loop will be correct when i holds the correct number
-			// below the values are retrieved from the input boxes
+		var financialArray = [];
+		// Iterate through all the visible Debt paragraphs
+		$('#p_scents p').each(function(){
+			var debtName = $(this).children('.debtName').val();
+			var debtAmount = parseFloat($(this).children('.debtAmount').val());
+			var debtInterestRate = parseFloat($(this).children('.interestRate').val());
+			var minMonthlyPayment = parseFloat($(this).children('.minMonthlyPayment').val());
 
-			var dnameVar = $('input[name=\"dname_' + z + '\"]').val();
-			//alert("debt namelength: " + dnameVar.length)
-			var amountVar = parseFloat($('input[name=\"amount_' + z + '\"]').val());
-			var interestVar = parseFloat($('input[name=\"interest_' + z + '\"]').val());
-			var minPayVar = parseFloat($('input[name=\"minpayamount_' + z + '\"]').val());
-			// alert("dnamevar: " + dnameVar);
-			// alert("amountvar " + amountVar);
-			// alert("interestVar " + interestVar);
-			// alert("minVar " + minPayVar);
-
-			if (dnameVar.length == 0 & isNaN(amountVar) & isNaN(interestVar) & isNaN(minPayVar)) {
-				alert("Nothing on this row has anything in it");
-				continue;
-			};
-
-			if (dnameVar.length > 0 & (isNaN(amountVar) | isNaN(interestVar) | isNaN(minPayVar))) {
-
-				if (isNaN(interestVar)) {
-					$('input[name="interest_' + z + '"]').addClass('error');
-					alert("Please make sure that the highlighted cells are in number format");
-				};
-				if (isNaN(amountVar)) {
-					$('input[name="amount_' + z + '"]').addClass('error');
-					alert("Please make sure that the highlighted cells are in number format");
-				};
-				if (isNaN(minPayVar)) {
-					$('input[name="minpayamount_' + z + '"]').addClass('error');
-					alert("Please make sure that the highlighted cells are in number format");
-				};
-
-				break;
-				// TODO perform this check for amountVar and InterestVar instead
-
-			};
-			if (dnameVar.length == 0 & (!isNaN(amountVar) | !isNaN(interestVar) | !isNaN(minPayVar))) {
-
-				$('input[name="dname_' + z + '"]').addClass('error');
-				alert("Please make sure that the highlighted cell holds the name for its respective row");
-				break;
-			};
-			// get the other inputs in the same row if the first input has something
-			// add the values of each row to the array
-
-			//alert(dnameVar + amountVar + interestVar + minPayVar);
-
-			//alert("Got to array");
-			financialArray[arrayRowCounter] = {
-				name : dnameVar,
-				amount : amountVar * -1,
-				rate : interestVar,
-				minPay : minPayVar
-			};
-
-			// the array is sorted according to AMOUNT(Should this be something else?)
-			financialArray = financialArray.sort(function(a, b) {
-				return a[1] > b[1];
+			financialArray.push({
+				name: debtName,
+				amount: debtAmount * -1,
+				rate: debtInterestRate,
+				minPay: minMonthlyPayment
 			});
-			console.log(financialArray);
-			var total = financialArray[arrayRowCounter][1] + financialArray[arrayRowCounter][2] + financialArray[arrayRowCounter][3];
+		});
 
-			// alert("Array sum: "+ total);
-			arrayRowCounter++;
-			// don't forget to convert this into a decimal
-			// as long as the rows hold the right inputs, it doesn't matter what order these are stored in
-			// else
-		};// for loop
-		 //alert("Reached End of Inputs, Going to About Page");
+		// the array is sorted according to AMOUNT(Should this be something else?)
+		financialArray = financialArray.sort(function(a, b) {
+			return a[1] > b[1];
+		});
 
-		// document.location.href ="/about";
-		// this will change to whatever the calculation page is
+		console.log(financialArray);
 
+		// TODO Need to review this against an external site that does the same
+		// TODO Make this its own function, for organization
 		var storedArray = [];
 
-		//var payment = parseFloat(document.getElementsByName("income_input").value) / 12 * .01;
-		//alert("This is your income-based payment: "+ payment);
-		//this accumulates each iteration, at the beginning we assume that the amount(10) is 1% of the person's income
+		// This accumulates each iteration, at the beginning we assume
+		// that the amount(10) is 1% of the person's income
 		var futureValue = 0;
-		//this doesn't change and is only used in the nper function
+
+		// This doesn't change and is only used in the nper function
 		var totalMonthsPassed = 0;
-		//this adds the nper calculations per iteration, plus the special "rollover" payment month when one debt is finished and the difference is applied to the next
+
+		// This adds the nper calculations per iteration, plus the
+		// special "rollover" payment month when one debt is finished
+		// and the difference is applied to the next
 		var finalMonthRolloverAmount = 0;
-		// this holds the unique value applied to the next debt during rollover month
+
+		// This holds the unique value applied to the next debt during rollover month
 		payment = payment + financialArray[0]["minPay"];
 		var currentDebtAmount = 0;
 		var payoffMonths;
@@ -192,7 +203,7 @@ $(document).ready(function() {
 			};
 			console.log("Debt: " + financialArray[i]["name"] + " Amount: " + financialArray[i]["amount"]);
 
-			payoffMonths = nper(parseFloat(financialArray[i]["rate"]), 12, payment, financialArray[i]["amount"], futureValue);
+			payoffMonths = FC.calculate.nper(parseFloat(financialArray[i]["rate"]), 12, payment, financialArray[i]["amount"], futureValue);
 
 			//we round down in order to perform the fv function
 			var roundedPayoffMonths = Math.floor(payoffMonths);
@@ -218,7 +229,7 @@ $(document).ready(function() {
 			totalMonthsPassed = totalMonthsPassed + roundedPayoffMonths;
 
 			//this calculates the last month amount and then adds the difference to the minPay of the next debt
-			var lastMonthAmount = fv(parseFloat(financialArray[i]["rate"]), 12, roundedPayoffMonths, payment, financialArray[i]["amount"]);
+			var lastMonthAmount = FC.calculate.fv(parseFloat(financialArray[i]["rate"]), 12, roundedPayoffMonths, payment, financialArray[i]["amount"]);
 			lastMonthAmount = lastMonthAmount * (1+(financialArray[i]["rate"]*.01));
 			console.log("The residual amount in the last month is for " + financialArray[i]["name"] + ": " + lastMonthAmount);
 			console.log("Rollover Amount: " + finalMonthRolloverAmount+ " going into array storage");
@@ -241,8 +252,7 @@ $(document).ready(function() {
 			finalMonthRolloverAmount = payment - (lastMonthAmount); //TODO account for interest in the last month
 
 			console.log("Rollover Amount: " + finalMonthRolloverAmount);
-			//function nper(rate, per, pmt, pv, fv)
-			//function fv(rate, per, nper, pmt, pv)
+
 			paymentSchedule[2] = {
 				"payment" : payment,
 				"months" : roundedPayoffMonths,
@@ -260,10 +270,10 @@ $(document).ready(function() {
 				break;
 			}
 			//here currentDebtAmount refers to the next loans value using the totalMonthPassed as the number of periods and 12 as NPER/YR
-			currentDebtAmount = fv(financialArray[i+1]["rate"], 12, totalMonthsPassed, financialArray[i+1]["minPay"], financialArray[i+1]["amount"]);
+			currentDebtAmount = FC.calculate.fv(financialArray[i+1]["rate"], 12, totalMonthsPassed, financialArray[i+1]["minPay"], financialArray[i+1]["amount"]);
 			console.log("The next debt's current amount at " + totalMonthsPassed + " months is " + currentDebtAmount);
 			//calculates next month's fv with rollover payment
-			var amountAfterRollover = fv(financialArray[i+1]["rate"], 12, 1, financialArray[i+1]["minPay"] + finalMonthRolloverAmount, (currentDebtAmount * -1));
+			var amountAfterRollover = FC.calculate.fv(financialArray[i+1]["rate"], 12, 1, financialArray[i+1]["minPay"] + finalMonthRolloverAmount, (currentDebtAmount * -1));
 			console.log("Amount after rollover payment month: " + amountAfterRollover);
 			//below, the original array is altered to hold the current amount for the next debt for the start of it's iteration
 			financialArray[i+1]["amount"] = amountAfterRollover * -1;
@@ -280,117 +290,7 @@ $(document).ready(function() {
 			console.log(storedArray[i]["paymentSchedule"]);
 		}//end for
 
-		function nper(rate, per, pmt, pv, fv) {
 
-			fv = parseFloat(fv);
-
-			pmt = parseFloat(pmt);
-
-			pv = parseFloat(pv);
-
-			per = parseFloat(per);
-
-			if ((per == 0 ) || (pmt == 0 )) {
-
-				alert("Why do you want to test me with zeros?");
-
-				return (0);
-
-			}
-
-			rate = eval((rate) / (per * 100));
-
-			if (rate == 0)// Interest rate is 0
-
-			{
-
-				nper_value = -(fv + pv) / pmt;
-
-			} else {
-
-				nper_value = Math.log((-fv * rate + pmt) / (pmt + rate * pv)) / Math.log(1 + rate);
-
-			}
-
-			nper_value = conv_number(nper_value, 2);
-
-			return (nper_value);
-
-		}
-
-		function conv_number(expr, decplaces) {// This function is from David Goodman's Javascript Bible.
-
-			var str = "" + Math.round(eval(expr) * Math.pow(10, decplaces));
-
-			while (str.length <= decplaces) {
-
-				str = "0" + str;
-
-			}
-
-			var decpoint = str.length - decplaces;
-
-			return (str.substring(0, decpoint) + "." + str.substring(decpoint, str.length));
-
-		}
-
-		//futureValue Function//////////////////////////////////////////////
-
-		function fv(rate, per, nper, pmt, pv) {
-
-			nper = parseFloat(nper);
-
-			pmt = parseFloat(pmt);
-
-			pv = parseFloat(pv);
-
-			rate = eval((rate) / (per * 100));
-
-			if ((pmt == 0 ) || (nper == 0 )) {
-
-				alert("Why do you want to test me with zeros?");
-
-				return (0);
-
-			}
-
-			if (rate == 0)// Interest rate is 0
-
-			{
-
-				fv_value = -(pv + (pmt * nper));
-
-			} else {
-
-				x = Math.pow(1 + rate, nper);
-
-				// y = Math.pow(1 + rate, nper);
-
-				fv_value = -(-pmt + x * pmt + rate * x * pv ) / rate;
-
-			}
-
-			fv_value = conv_number(fv_value, 2);
-
-			return (fv_value);
-
-		}
-
-		function conv_number(expr, decplaces) {// This function is from David Goodman's Javascript Bible.
-
-			var str = "" + Math.round(eval(expr) * Math.pow(10, decplaces));
-
-			while (str.length <= decplaces) {
-
-				str = "0" + str;
-
-			}
-
-			var decpoint = str.length - decplaces;
-
-			return (str.substring(0, decpoint) + "." + str.substring(decpoint, str.length));
-
-		}
 
 	}); // eventListener
 
